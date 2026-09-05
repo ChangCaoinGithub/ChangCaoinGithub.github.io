7de3bd9a6dba2d33806fcaf7ef35e103fb603117
ChangCaoinGithub <119680422+ChangCaoinGithub@users.noreply.github.com>
2026-09-05 17:39:03 +0200

    Update page.tsx

--- app/travel/page.tsx
+++ app/travel/page.tsx
@@ -3,7 +3,7 @@ import { RealTravelMap } from "./RealTravelMap";
 
 export default function TravelPage() {
   return <main><SiteHeader active="travel" /><section className="direct-page shell"><RealTravelMap /></section><section className="page-section shell fact-grid">
-    <article><span></span><h2>Food-driven</h2><p>And actually I also enjoy exploring the chinese restaurant in local.</p></article>
+    <article><span></span><h2>Food-driven</h2><p>Besides trying local cuisine, as a Chinese person, I also enjoy exploring Chinese restaurants around the world.</p></article>
     <article><span></span><h2>Beach lover</h2><p>I often visit different seas or lakes, and I so love soft sand beach, but weirdly, I do not like swimming at all!!</p></article>
     </section></main>;
 }
